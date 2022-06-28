613ff0edb992ead8f2f3dcfb489c5f6547a450cc
Your Name <you@example.com>
2022-06-28 15:20:01 +0200

    Testcode removed

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { LoadingController } from '@ionic/angular';
-import { GoogleAuthProvider } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-  constructor(private afAuth: AngularFireAuth, private loader: LoadingController) { }
-
-  ngOnInit() {
-  }
-
-  async loginTest() {
-    await this.afAuth.signInWithPopup(new GoogleAuthProvider);
-
-  }
+export class LoginPage  {
+  constructor(private afAuth: AngularFireAuth) { }
 }
